6ea490b2893c7f2548982af7ceabb3a63b4282e7
Rakesh Ravindran <rmravindran@uber.com>
2024-01-13 07:59:53 -0500

    stash update

--- src/boost/core/symtable.go
+++ src/boost/core/symtable.go
@@ -2,37 +2,203 @@ package core
 
 import (
 	"errors"
-
-	"github.com/m3db/m3/src/dbnode/generated/proto/index"
 )
+
+type AttributeEncoding int
+
+const (
+	// Values are encoded as raw unsigned values
+	UnsignedNumericValue AttributeEncoding = iota
+
+	// Map values to indices in a dictionary
+	DictionaryEncodedValue
+)
+
+type AttributeTable struct {
+	name                   string
+	encodingType           AttributeEncoding
+	encodedValues          []uint64
+	encodedValuesFromIndex map[uint64]uint64
+}
 
 type SymTable struct {
 	name string
 
-	dict map[int]string
+	// Dictionary encoded entries of the attributes
+	dictToString map[uint64]string
+	dictToIndex  map[string]uint64
+
+	// Header, aka the column index
+	header map[string]int
+
+	// Attribute Tables
+	attributeTable map[string]*AttributeTable
 }
 
-func (sym *SymTable) NewSymTable(name string) *SymTable {
+func NewSymTable(name string) *SymTable {
 	return &SymTable{
-		name: name,
-		sym.dict : make(map[int]string),
+		name:           name,
+		dictToString:   make(map[uint64]string),
+		dictToIndex:    make(map[string]uint64),
+		header:         make(map[string]int),
+		attributeTable: make(map[string]*AttributeTable),
 	}
 }
 
-func (sym *SymTable) UpdateDictioinary(indices []int, attributeNames []string) error {
-	if len(indices) == 0 || len(indices) != len(attributeNames) {
+// Returns the name of the symbol table
+func (sym *SymTable) Name() string {
+	return sym.name
+}
+
+// Returns the number of symbols in the symbol table
+func (sym *SymTable) NumSymbols() int {
+	return len(sym.dictToString)
+}
+
+// Update the dictionary with the given indices and attribute values. If the
+// index already exists in the dictionary, or the attribute name already exists
+// in the dictionary, this is an error
+func (sym *SymTable) UpdateDictionary(indices []uint64, attributeValues []string) error {
+	if len(indices) == 0 || len(indices) != len(attributeValues) {
 		return errors.New("indices and values must of the same size")
 	}
-	
+
 	for i, indexValue := range indices {
-		attributeName := attributeNames[i]
-		
-		if _, ok := sym.dict[indexValue]; ok {
+		attributeValue := attributeValues[i]
+
+		if _, ok := sym.dictToString[indexValue]; ok {
 			return errors.New("index value already exists in symbol table")
 		}
-		
-		sym.map[indexValue] = attributeName
+
+		if _, ok := sym.dictToIndex[attributeValue]; ok {
+			return errors.New("attribute name already exists in symbol table")
+		}
+
+		sym.dictToString[indexValue] = attributeValue
+		sym.dictToIndex[attributeValue] = indexValue
 	}
 
 	return nil
+}
+
+// Returns true if the given attribute value already exists in the symbol table
+func (sym *SymTable) AttributeValueExists(value string) bool {
+	_, ok := sym.dictToIndex[value]
+	return ok
+}
+
+// Inserts the given attribute value into the attribute having the specified
+// name. If the attribute name doesn't exist, a new attribute is created in the
+// SymTable. If the value already exists, this is a NOP
+func (sym *SymTable) InsertAttributeValue(name string, value string) error {
+
+	if _, ok := sym.dictToIndex[value]; !ok {
+		id := uint64(len(sym.dictToIndex))
+		sym.dictToIndex[value] = id
+		sym.dictToString[id] = value
+	}
+
+	if _, ok := sym.attributeTable[name]; !ok {
+		sym.attributeTable[name] = &AttributeTable{
+			name:                   name,
+			encodingType:           DictionaryEncodedValue,
+			encodedValues:          make([]uint64, 0, 10),
+			encodedValuesFromIndex: make(map[uint64]uint64),
+		}
+		sym.header[name] = len(sym.header)
+	}
+
+	// If the value is not already part of the attribute, make it
+	id := sym.dictToIndex[value]
+	if _, ok := sym.attributeTable[name].encodedValuesFromIndex[id]; !ok {
+		sym.attributeTable[name].encodedValues = append(sym.attributeTable[name].encodedValues, id)
+		sym.attributeTable[name].encodedValuesFromIndex[id] = uint64(len(sym.attributeTable[name].encodedValuesFromIndex))
+	}
+
+	return nil
+}
+
+// Insert the attribute values denoted by the given attribute indices into the
+// attribute having the specified name. If the attribute indices are not valid
+// (i.e. they don't exist in the symbol table), this is an error.
+func (sym *SymTable) InsertAttributeIndices(name string, indices []uint64) error {
+	if _, ok := sym.attributeTable[name]; !ok {
+		sym.attributeTable[name] = &AttributeTable{
+			name:                   name,
+			encodingType:           DictionaryEncodedValue,
+			encodedValues:          make([]uint64, 0, 10),
+			encodedValuesFromIndex: make(map[uint64]uint64),
+		}
+		sym.header[name] = len(sym.header)
+	}
+
+	// Do a sanity check to make sure the indices are valid	before touching
+	// the symbol table
+	for _, index := range indices {
+		if _, ok := sym.dictToString[index]; !ok {
+			return errors.New("index value doesn't exist in symbol table")
+		}
+	}
+
+	// Update the foward and reverse mapping
+	for _, index := range indices {
+		sym.attributeTable[name].encodedValues = append(sym.attributeTable[name].encodedValues, index)
+		sym.attributeTable[name].encodedValuesFromIndex[index] = uint64(len(sym.attributeTable[name].encodedValuesFromIndex))
+	}
+
+	return nil
+}
+
+// Find the index of the given attribute value. If the attribute value doesn't
+// exist, returns -1
+func (sym *SymTable) FindAttributeIndex(name string, value string) int {
+	if _, ok := sym.attributeTable[name]; !ok {
+		return -1
+	}
+
+	dictIndex, ok := sym.dictToIndex[value]
+	if !ok {
+		return -1
+	}
+
+	if val, ok := sym.attributeTable[name].encodedValuesFromIndex[dictIndex]; ok {
+		return int(val)
+	}
+
+	return -1
+}
+
+// Find the attribute value for the given index. If the index doesn't exist,
+// returns an empty string
+func (sym *SymTable) FindAttributeValue(name string, index uint64) string {
+	if _, ok := sym.attributeTable[name]; !ok {
+		return ""
+	}
+
+	if index >= uint64(len(sym.attributeTable[name].encodedValues)) {
+		return ""
+	}
+
+	dictIndex := sym.attributeTable[name].encodedValues[index]
+	value, ok := sym.dictToString[dictIndex]
+	if !ok {
+		return ""
+	}
+
+	return value
+}
+
+// Return the index header for the given set of attributes
+func (sym *SymTable) GetIndexedHeader(attributes map[string]string) []int {
+	header := make([]int, len(sym.header))
+
+	for name, i := range sym.header {
+		header[i] = -1
+		val, ok := attributes[name]
+		if ok {
+			header[i] = sym.FindAttributeIndex(name, val)
+		}
+	}
+
+	return header
 }
---
+++ src/boost/core/symtable_test.go
@@ -0,0 +1,138 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSymTableUpdateDictionary(t *testing.T) {
+
+	symTable := NewSymTable("test")
+	require.NotNil(t, symTable)
+
+	indices := []uint64{1, 2, 3, 4, 5}
+	attributeValues := []string{"a", "b", "c", "d", "e"}
+
+	err := symTable.UpdateDictionary(indices, attributeValues)
+	require.NoError(t, err)
+
+	// Test that the dictionary is updated correctly
+	require.True(t, symTable.AttributeValueExists("a"))
+	require.True(t, symTable.AttributeValueExists("b"))
+	require.True(t, symTable.AttributeValueExists("c"))
+	require.True(t, symTable.AttributeValueExists("d"))
+	require.True(t, symTable.AttributeValueExists("e"))
+
+	// Test something that doesn't exist
+	require.False(t, symTable.AttributeValueExists("f"))
+
+	// Adding something that already exists should fail
+	err = symTable.UpdateDictionary([]uint64{6}, []string{"a"})
+	require.Error(t, err)
+	err = symTable.UpdateDictionary([]uint64{5}, []string{"x"})
+	require.Error(t, err)
+}
+
+func TestSymTableAttributes(t *testing.T) {
+	symTable := NewSymTable("test")
+	require.NotNil(t, symTable)
+
+	indices := []uint64{0, 1, 2, 3, 4}
+	attributeValues := []string{"a", "b", "c", "d", "e"}
+	err := symTable.UpdateDictionary(indices, attributeValues)
+	require.NoError(t, err)
+
+	// Create an attribute
+	symTable.InsertAttributeValue("host", "a")
+	require.Equal(t, 0, symTable.FindAttributeIndex("host", "a"))
+	// value "b" hasn't yet mapped to the host attribute
+	require.Equal(t, -1, symTable.FindAttributeIndex("host", "b"))
+	// now map it. Observe that the index should be continuous from the previous
+	// index
+	symTable.InsertAttributeValue("host", "b")
+	require.Equal(t, 1, symTable.FindAttributeIndex("host", "b"))
+	// and verify that previous index hasn't changed.
+	require.Equal(t, 0, symTable.FindAttributeIndex("host", "a"))
+
+	// Find the attribute value for the given index
+	require.Equal(t, "a", symTable.FindAttributeValue("host", 0))
+	require.Equal(t, "b", symTable.FindAttributeValue("host", 1))
+	// Asking for a non-existent index should return an empty string
+	require.Equal(t, "", symTable.FindAttributeValue("host", 2))
+	require.Equal(t, "", symTable.FindAttributeValue("host", 3))
+	require.Equal(t, "", symTable.FindAttributeValue("host", 4))
+
+	// Get the indexed header. It should have just 1 entry since we only
+	// have 1 attribute
+	indexedHeader := symTable.GetIndexedHeader(map[string]string{"host": "a"})
+	require.Equal(t, 1, len(indexedHeader))
+	require.Equal(t, 0, indexedHeader[0])
+	// Asking for a non-existent attribute should return a header with -1
+	indexedHeader = symTable.GetIndexedHeader(map[string]string{"crap": "a"})
+	require.Equal(t, 1, len(indexedHeader))
+	require.Equal(t, -1, indexedHeader[0])
+	// Asking for a non-existent value should return a header with -1
+	indexedHeader = symTable.GetIndexedHeader(map[string]string{"host": "crap"})
+	require.Equal(t, 1, len(indexedHeader))
+	require.Equal(t, -1, indexedHeader[0])
+}
+
+func TestSymTableMultipleAttributesSharingIndex(t *testing.T) {
+	symTable := NewSymTable("test")
+	require.NotNil(t, symTable)
+
+	// host, src, dst all share the same universe of values
+
+	indices := []uint64{0, 1, 2, 3, 4, 5, 7, 8, 9, 10}
+	attributeValues := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+
+	err := symTable.UpdateDictionary(indices, attributeValues)
+	require.NoError(t, err)
+
+	// Add all attribute values as host attribute values
+	for ndx, v := range attributeValues {
+		symTable.InsertAttributeValue("host", v)
+		require.Equal(t, ndx, symTable.FindAttributeIndex("host", v))
+	}
+
+	// Add some of the attribute values as src attribute values
+	// Note that each attribute has its own index space starting at 0.
+	for ndx, v := range attributeValues[0:5] {
+		symTable.InsertAttributeValue("src", v)
+		require.Equal(t, ndx, symTable.FindAttributeIndex("src", v))
+	}
+
+	// Add remaining attribute values as dst attribute values
+	for ndx, v := range attributeValues[5:] {
+		symTable.InsertAttributeValue("dst", v)
+		require.Equal(t, ndx, symTable.FindAttributeIndex("dst", v))
+	}
+
+	// Check the attribute values for the host attribute
+	for ndx, v := range attributeValues {
+		require.Equal(t, v, symTable.FindAttributeValue("host", uint64(ndx)))
+	}
+	// Check the attribute values for the src attribute
+	for ndx, v := range attributeValues[0:5] {
+		require.Equal(t, v, symTable.FindAttributeValue("src", uint64(ndx)))
+	}
+	// Check the attribute values for the dst attribute
+	for ndx, v := range attributeValues[5:] {
+		require.Equal(t, v, symTable.FindAttributeValue("dst", uint64(ndx)))
+	}
+
+	// Check the indexed data, it should have 3 entries all 0 since we are
+	// asking for the first value encoded in the insert operation for each
+	// attribute
+	indexedHeader := symTable.GetIndexedHeader(
+		map[string]string{
+			"host": "a",
+			"src":  "a",
+			"dst":  "f",
+		})
+	require.Equal(t, 3, len(indexedHeader))
+	require.Equal(t, 0, indexedHeader[0])
+	require.Equal(t, 0, indexedHeader[0])
+	require.Equal(t, 0, indexedHeader[0])
+}
